550e3189abfb136a6f0e8efc1d717149b44dfcea
Umur Burhan Yavuz <umur@binnaz.com>
2019-06-28 01:33:27 +0300

    Initial Commit

---
+++ Nerde Yesem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nerde Yesem
+//
+//  Created by umur yavuz on 28.06.2019.
+//  Copyright © 2019 umur yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
